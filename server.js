fd91555414ea4095bdb83fb6a4db072287db111b
Marta Niemiec <niemiec.marta@hotmail.com>
2019-04-11 14:06:51 +0200

    done register and signin with database and bcrypt

--- server.js
+++ server.js
@@ -22,97 +22,67 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// app.use(bodyParser.urlencoded({extended: false}));
-// app.use(bodyParser.json());
-
-const db = {
-  users: [
-    
-    {
-      id: '123',
-      name: 'john',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      // tasks: [{task: "costam"}],
-      allTasks: {
-        dailyTasks: [], 
-        weeklyTasks: [], 
-        monthlyTasks: []
-      },
-      skills: [],
-      registered: new Date()
-    },
-    {
-      id: '124',
-      name: 'sally',
-      email: 'sally@gmail.com',
-      password: 'bananas',
-      // tasks: [{task: "costam"}],
-      allTasks: {
-        dailyTasks: [], 
-        weeklyTasks: [], 
-        monthlyTasks: []
-      },
-      skills: [],
-      registered: new Date()
-    }
-  ],
-  login: [
-    {
-      id: '999',
-      hash: '',
-      email: 'john@gmail.com'
-    }
-  ]
-}
 
 app.get('/', (req, res) => {
-  res.send(db.users)
+  // res.send(db.users)
 })
 
 
 app.post('/signin', (req, res) => {
-
-    // Load hash from your password DB.
-  bcrypt.compare("apple", "$2a$10$9eBsCQ7w8nG0XWFamAvZb.HqJMIDaJwxorjL7XTm7XmcREeiWNLKC", function(err, res) {
-    // res == true
-    // console.log('first' ,res);
-  });
-  bcrypt.compare("veggies", "$2a$10$9eBsCQ7w8nG0XWFamAvZb.HqJMIDaJwxorjL7XTm7XmcREeiWNLKC", function(err, res) {
-    // res = false
-    // console.log("second", res);
-  });
-
-  if (req.body.email === db.users[0].email &&
-      req.body.password === db.users[0].password) {
-    // res.json('success')
-    res.json(db.users[0])
-  } else {
-    // console.log(req.body.email);
-    // console.log(req.body.password);
-    res.status(400).json('error logging in')
-  }
+  database.select('email', 'hash').from('login')
+  .where('email', '=', req.body.email)
+  .then(data => {
+    const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+    if (isValid) {
+      return database.select('*').from('users')
+      .where('email', '=', req.body.email)
+      .then(user => {
+        res.json(user[0])
+      })
+      .catch(err => res.status(400).json('unable to get user'))
+    }
+    else {
+      res.status(400).json('wrong credentials')
+    }
+  })
+  .catch(err => res.status(400).json('wrong credentials'))
 })
 
 
 app.post('/register', (req, res) => {
   //get information from the req.body
   const { email, name, password } = req.body;
-  bcrypt.hash(password, null, null, function(err, hash) {
-    console.log(hash);
-  });
+  const hash = bcrypt.hashSync(password);
   // push the user into database
-  database('users')
-  .returning('*')
-  .insert({
-    name: name,
-    email: email,
-    allTasks: {},
-    skills: [],
-    registered: new Date()
-  }).then(user => {
-    res.json(user[0])
-  }).catch(err => res.status(400).json('unable to register'))
+  database.transaction(trx => { //transaction to do more than one thing at once; then "trx" is instead of "database" to do all operations
+    trx.insert({
+      hash: hash,
+      email: email
+    })
+    .into('login')  // inserted hash and email into "login"
+    .returning('email') // returned an email
+    .then(loginEmail => { // used loginEmail to do another trx transaction to insert into the "users" all following informations 
+      return trx('users')
+      .returning('*')
+      .insert({
+        name: name,
+        email: loginEmail[0],
+        allTasks: {},
+        dailytasks: [],
+        weeklytasks: [],
+        monthlytasks: [],
+        skills: [],
+        registered: new Date()
+      }).then(user => {
+        res.json(user[0]);  // respond with json,
+      })
+    })
+    .then(trx.commit) // trx.commit is for adding all above informations
+    .catch(trx.rollback)  // trx.rollback when anything fails
+  })
+
+ 
+  .catch(err => res.status(400).json('unable to register'))
 })
 
 
@@ -135,24 +105,22 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/alltasks', (req, res) => {
   const { id } = req.body;  
-  const { dailyTasks, weeklyTasks, monthlyTasks, skills } = req.body;
+  const { dailytasks, weeklytasks, monthlytasks, skills } = req.body;
 
-  let found = false;
-
-  db.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      user.allTasks = {};
-      user.allTasks.dailyTasks = dailyTasks;
-      user.allTasks.weeklyTasks = weeklyTasks;
-      user.allTasks.monthlyTasks = monthlyTasks;
-      user.skills = skills;
-      return res.json(user);
-    } 
-  })
-  if (!found) {
-    res.status(400).json('not found user with tasks');
-  }
+  database('users').where('id', '=', id)
+    .update({
+      allTasks: {},
+      dailytasks: dailytasks,
+      weeklytasks: weeklytasks,
+      monthlytasks: monthlytasks,
+      skills: skills
+    })
+    .returning(['allTasks','dailytasks', 'weeklytasks', 'monthlytasks', 'skills'])
+    .then(data => {
+      console.log(data);
+      res.json(data[0]);
+    })
+    .catch(err => res.status(400).json('unable to get data'))
 });
 
 
